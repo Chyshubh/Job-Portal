1092f65f9f1af27d3d0abb2cdb45b14740b5267c
Chyshubh <chy.shubh@gmail.com>
2024-12-28 01:27:36 +0530

    Initial commit

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import AppContext from '../context/AppContext'
+import { assets } from '../assets/assets/assets';
 
 const JobListing = () => {
+
+    const {searchFilter, isSearched, s} = useContext(AppContext);
   return (
-    <div>JobListing</div>
+    <div>
+
+        {/*Sidebar */}
+        <div className="">
+            {/*Search Filter from Hero Component*/}
+            {
+                isSearched && (searchFilter.title !== '' || searchFilter.location !== '') &&
+                <>
+                <h3 className="">Current Search</h3>
+                <div className="">
+                    {searchFilter.title &&(
+                            <span className="">
+                                {searchFilter.title}
+                                <img src={assets.cross_icon} alt="" className="cursor-pointer" />
+                            </span>
+                        )}
+                    {searchFilter.location &&(
+                            <span className="">
+                               {searchFilter.location} 
+                               <img src={assets.cross_icon} alt="" className="cursor-pointer" />
+                            </span>
+                        )}
+                </div>
+                </>
+            }
+        </div>
+    </div>
   )
 }
 
